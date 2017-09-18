016b3c931307857e578f22c16288be40cd587aef
benjamindehli <superelg@gmail.com>
2017-09-18 22:26:08 +0200

    Adjust GUI

--- src/components/examples/Buttons.vue
+++ src/components/examples/Buttons.vue
@@ -1,36 +1,45 @@
 <template>
 	<div>
 		<div class="generator">
-			<div class="config">
-				<div><label>id<input v-model="elementProperties.id" /></label></div>
-				<div><label>link<input v-model="elementProperties.link" /></label></div>
-				<div><label>iconClass<input v-model="elementProperties.iconClass" /></label></div>
-				<div><label>content<input v-model="elementProperties.content" /></label></div>
-				<div><label>submit<input type="checkbox" v-model="elementProperties.submit" /></label></div>
+			<h2>Button</h2>
+			<div class="row">
+				<div class="config col-sm-4">
+					<h3>Properties</h3>
+					<div><label>ID<input v-model="elementProperties.id" /></label></div>
+					<div><label>Link<input v-model="elementProperties.link" /></label></div>
+					<div><label>Icon class<input v-model="elementProperties.iconClass" /></label></div>
+					<div><label>Content<input v-model="elementProperties.content" /></label></div>
+					<div><label>Submit<input type="checkbox" v-model="elementProperties.submit" /></label></div>
 
-				<div><label>type<select v-model="elementProperties.type">
-					<option v-for="type in config.type" v-bind:value="type">{{ type }}</option>
-				</select></label></div>
+					<div><label>Type<select v-model="elementProperties.type">
+						<option v-for="type in config.type" v-bind:value="type">{{ type }}</option>
+					</select></label></div>
 
-				<div>
-					<label>theme
-						<select v-model="elementProperties.theme">
-							<option v-for="theme in config.theme" v-bind:value="theme">{{ theme }}</option>
-						</select>
-					</label>
+					<div>
+						<label>Theme
+							<select v-model="elementProperties.theme">
+								<option v-for="theme in config.theme" v-bind:value="theme">{{ theme }}</option>
+							</select>
+						</label>
+					</div>
 				</div>
-			</div>
 
-			<div class="result">
-				<div class="visual">
-					<div v-html="buttonElement"></div>
-				</div>
-				<div class="markup">
-					<span>import * as quark from 'quark-gui';</span>
-					<span>let Button = quark.Atoms.Buttons.Button;</span>
-					<span>Button.getModule(</span>
-					<pre v-html="elementProperties"></pre>
-					<span>);</span>
+				<div class="result col-sm-8">
+					<h3>Result</h3>
+					<div class="tabs">
+						<span class='tab tab-visual' v-bind:class="{active: resultOptions.show == 'visual'}" v-on:click="resultOptions.show = 'visual'">Visual</span>
+						<span class='tab tab-markup' v-bind:class="{active: resultOptions.show == 'markup'}" v-on:click="resultOptions.show = 'markup'">Markup</span>
+					</div>
+					<div v-show="resultOptions.show == 'visual'" class="visual">
+						<div v-html="buttonElement"></div>
+					</div>
+					<div v-show="resultOptions.show == 'markup'" class="markup">
+						<span>import * as quark from 'quark-gui';</span>
+						<span>let Button = quark.Atoms.Buttons.Button;</span>
+						<span>Button.getModule(</span>
+						<pre v-html="elementProperties"></pre>
+						<span>);</span>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -51,7 +60,7 @@ export default {
 				id: '',
 				link: '',
 				iconClass: '',
-				content: '',
+				content: 'button',
 				title: '',
 				type: 'flat',
 				theme: 'default',
@@ -60,6 +69,9 @@ export default {
 			config: {
 				theme: ['default', 'primary', 'info', 'success', 'warning', 'danger'],
 				type: ['flat', 'raised', 'minimal']
+			},
+			resultOptions:{
+				show: 'visual'
 			}
 
 		}
